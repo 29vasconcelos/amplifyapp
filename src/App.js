308b3862dcb7d243e8205057bf8411147b6fbcba
29vasconcelos <29vasconcelos@gmail.com>
2021-11-13 12:27:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>Por joão Vasconcelos Simão</h1>
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
